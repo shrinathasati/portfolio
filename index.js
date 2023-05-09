e2048c77ab5a0a3f8ad54b0bd131528718dc74cc
Shrinath Asati <120880974+shrinathasati@users.noreply.github.com>
2023-05-09 22:17:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function geeksforgeeks() {
+    var x = document.getElementById("menus");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+// Function to toggle the plus menu into minus
+function myFunction(x) {
+    x.classList.toggle("fa-minus-circle");
+}
